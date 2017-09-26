2e11f52c061c3a33c3531439202a8727cbbe397f
Jakub Petriska <jakubpetriska@gmail.com>
2017-09-27 00:25:49 +0200

    Remove non-peppy stuff, fix messages and comments

--- tweak.sh
+++ tweak.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 #
-# Script to Fix and apply Tweaks to Chromebooks running Linux
+# Linux keys remapping script for Chromebook Peppy
 #
-
-# get CB model codename
-CBCODENAME=$(sudo dmidecode -s system-product-name)
-
-fix_sound() {
-  ## for Baytrail TCB2
-  # stop sound service
-  sudo alsa force-unload
-
-  # make backup of original sound.state file
-  sudo cp -n /var/lib/alsa/asound.state /var/lib/alsa/asound.state.bck
-
-  # copy new sound.state file
-  sudo cp asound.state /var/lib/alsa
-}
 
 fix_keyboard_keys() {
 	# make backup of original pc config file
@@ -29,19 +14,9 @@ fix_keyboard_keys() {
 	sudo rm -rf /var/lib/xkb/*
 }
 
-# check for Baytrail CBs
-if [ "$CBCODENAME" == "Swanky" ]
-then
-	fix_sound
-
-  echo "*******************************************************"
-  echo "Fixed sound for Baytrail Chromebook"
-fi
-
 # apply keyboard remapping
 fix_keyboard_keys
-echo "*******************************************************"
-echo "Remapped top row media keys"
+echo "Keys remapped"
 
 ## reboot
 echo "*******************************************************"
